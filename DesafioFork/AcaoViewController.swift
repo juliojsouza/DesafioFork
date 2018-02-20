c3be330efbb4cc44234ca037b08ab86e8370222a
Júlio Souza <juliojsouza2@globo.com>
2018-02-20 20:37:50 -0300

    Segunda alteração

---
+++ DesafioFork/AcaoViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AcaoViewController.swift
+//  DesafioFork
+//
+//  Created by Júlio Souza on 20/02/2018.
+//  Copyright © 2018 Calebe Nunes Pastor. All rights reserved.
+//
+
+import UIKit
+
+class AcaoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+        
+        let acaoOK = UIAlertAction(title: "OK", style: .default, handler: nil
+        alerta.addAction(acaoOK)
+        present(alerta,animated: true, completion: nil)
+        
+    }
+
+}
